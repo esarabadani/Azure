13602a3f03c6c46437ec0fa6498e0797121456eb
Esmaeil Sarabadani <e.sarabadani@gmail.com>
2017-12-02 14:45:28 +0100

    v1.0

---
+++ Handy_Scripts/Add-User-to-Every-Azure-Subscription.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount -Credential $Credential
+Connect-MsolService -Credential $Credential 
+
+$subs = Get-AzureRmSubscription
+
+foreach($sub in $subs)
+{
+    $subID = $sub.SubscriptionId
+
+    Write-Output("`nSubscription Name: " + $sub.SubscriptionName + "`n")
+
+    Select-AzureRmSubscription -SubscriptionId $subID
+
+    $substring = "/subscriptions/" + $subID
+
+    New-AzureRmRoleAssignment -SignInName esmaeil@example.com -RoleDefinitionName owner -Scope $substring
+
+    Write-Output("`User is added `n")
+}
