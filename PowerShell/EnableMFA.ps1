aadec2333f4c0d408085c06def4292b33e07709f
Esmaeil Sarabadani <e.sarabadani@gmail.com>
2017-12-02 18:48:45 +0100

    me

---
+++ PowerShell/EnableMFA.ps1
@@ -0,0 +1,34 @@
+﻿#This script will enable Multi-Factor Autentication for all your Azure Users... This script also requires users to use MFA when using Office365
+
+Connect-MsolService
+
+Login-AzureRmAccount
+
+$subs = Get-AzureRmSubscription
+
+$st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$st.RelyingParty = "*"
+$st.State = “Disabled”
+$sta = @($st)
+
+foreach($sub in $subs)
+{
+    $subID = $sub.SubscriptionId
+
+    Write-Host("`nSubscription Name: " + $sub.SubscriptionName + "`n")
+
+    Select-AzureRmSubscription -SubscriptionId $subID
+
+    $substring = "/subscriptions/" + $subID
+
+    $usersList = (Get-AzureRmRoleAssignment -IncludeClassicAdministrators -scope $substring)
+
+    foreach($user in $usersList){
+
+        if($user.ObjectType -eq "User")
+        {
+            Set-MsolUser -UserPrincipalName $user.SignInName -StrongAuthenticationRequirements $sta
+        }
+    }
+}
+
