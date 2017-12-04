03622035836a6927f703772ac959bf2d8835768a
GRP\SarabadaniE <esmaeil.sarabadani@haufe-lexware.com>
2017-12-04 17:21:39 +0100

    Esmaeil

---
+++ Handy_Scripts/Get-Subscription-Users.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRmAccount
+
+$subs = Get-AzureRmSubscription
+
+foreach($sub in $subs)
+{
+    $subID = $sub.SubscriptionId
+    Write-Output("`nSubscription Name: " + $sub.SubscriptionName + "`n")
+    $substring = "/subscriptions/" + $subID
+    Get-AzureRmRoleAssignment -IncludeClassicAdministrators -scope $substring | FL SignInName
+}
